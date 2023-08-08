23b1f3380b7cc5e7365cb4153985f95d28f8a722
leahaeussermann <31536372+leahaeussermann@users.noreply.github.com>
2023-08-08 20:48:24 +0200

    Update CameraView.kt

--- android/src/main/java/com/mrousavy/camera/CameraView.kt
+++ android/src/main/java/com/mrousavy/camera/CameraView.kt
@@ -304,7 +304,6 @@ class CameraView(context: Context, private val frameProcessorThread: ExecutorSer
   override fun onDetachedFromWindow() {
     super.onDetachedFromWindow()
     updateLifecycleState()
-    lifecycleRegistry.currentState = Lifecycle.State.DESTROYED
   }
 
   /**
